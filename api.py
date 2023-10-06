055675fa74a22fcc5523c2782c6c856285f981b0
salik03 <uddinsalik03@gmail.com>
2023-10-06 23:31:55 +0530

    first commit

---
+++ api.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+def load_data():
+    with open("data.json", "r") as file:
+        data = json.load(file)
+    return data
+
+@app.route('/', methods=['GET']) 
+@cross_origin()
+def health():
+    return "Event Data API for Clubs&Chaps, route: /get_data"
+
+
+@app.route('/get_data', methods=['GET']) 
+@cross_origin()
+def get_data():
+    data = load_data()
+    
+    return data
+
+if __name__ == '__main__':
+    app.run()
